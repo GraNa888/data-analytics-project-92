53ceaba607bd543aa36f981c2ae6ebff423dd337
GraNa888 <138492077+GraNa888@users.noreply.github.com>
2023-07-05 13:23:50 +0300

    Add files via upload

---
+++ query.sql
@@ -0,0 +1,4 @@
+SELECT COUNT(*) AS customers_count 
+FROM customers; 
+--посчитай уникальные значения customer_id из таблицы customers и переименуй столбец в customers_count
+--SELECT COUNT(*) AS customers_count FROM customers; дополнительный запрос если нужно со значением NULL
