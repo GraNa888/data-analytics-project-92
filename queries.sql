6e4a0439240a3711532ab06776da3a2c25872001
GraNa888 <138492077+GraNa888@users.noreply.github.com>
2023-08-27 10:59:12 +0000

    Update lowest_average_income

--- queries.sql
+++ queries.sql
@@ -37,24 +37,25 @@ limit 10 --  отсортируй 10 позиций в порядке убыва
 
 
 lowest_average_income.csv 
-select concat ("first_name", ' ', "last_name") as name, --объедени first_name и last_name (с пробелом посередине) и назови столбец name в таблице employees
-round(sum(price)) as income --суммируй и округли до целых price и назови столбец income, для этого объедени таблицы sales и products по общему значению product_id 
+select 
+	concat ("first_name", ' ', "last_name") as name, --объедени first_name и last_name (с пробелом посередине) и назови столбец name в таблице employees
+	round(avg(price * quantity)) as average_income --найди среднне значение за сделку и назови столбец average_income, для этого объедени таблицы sales и products по общему значению product_id 
 from sales
-inner join employees
+inner join employees 
 on sales.sales_person_id = employees.employee_id
 inner join products 
 on sales.product_id = products.product_id
 group by first_name, last_name --сгруппируй по имени и фамилии и отсей
-having round(sum(price)) < (select avg(income) as average_income from( --округление по сумме до целых price МЕНЬШЕ при условии что выбран средний income и назван столбец average_income
+having round(avg(price * quantity)) < (select avg(average_income) as average_income from( --округление среднего до целых произведения price на quantity  МЕНЬШЕ при условии что выбран средний income и назван столбец average_income
   select --с помощью подзапроса выбери sales_person_id, sum(price) as income из таблицы products и присоедени по общему значению sales sales.product_id = products.product_id 
-  sales_person_id, sum(price) as income
+  sales_person_id, sum(price) as average_income
   from products
   inner join sales 
   on sales.product_id = products.product_id
   group by sales_person_id
 ) as a) --сгруппируй по sales_person_id и назави а
-order by income;
---отсортируй все по income по возрастанию
+order by average_income --отсортируй все по income по возрастанию
+;
 
 
 day_of_the_week_income.csv 
